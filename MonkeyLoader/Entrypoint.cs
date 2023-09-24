92f6219806412c0c1c6c461cdb647d54e0a5976e
Arne Kiesewetter <banane9@live.de>
2023-09-24 21:59:40 +0200

    Add project files.

---
+++ MonkeyLoader/Entrypoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Doorstop
+{
+    internal static class Entrypoint
+    {
+        public static void Start()
+        {
+            File.WriteAllText("doorstop_hello.log", "Hello from Unity!");
+        }
+    }
+}
