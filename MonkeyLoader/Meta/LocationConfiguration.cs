0eddcd10a345d80b6a643634ceaa4baa85a6e7b2
Arne Kiesewetter <banane9@live.de>
2023-10-06 16:48:05 +0200

    Add ModLoadingLocation

--- MonkeyLoader/Meta/LocationConfiguration.cs
+++ MonkeyLoader/Meta/LocationConfiguration.cs
@@ -12,12 +12,18 @@ namespace MonkeyLoader.Meta
     /// </summary>
     public sealed class LocationConfigSection : ConfigSection
     {
-        public static ConfigKey<List<string>> ConfigsKey = new("Configs", "Paths to check for configuration files.", () => new List<string>());
-        public static ConfigKey<List<string>> GamePacksKey = new("GamePacks", "Paths to check for game packs.", () => new List<string>());
-        public static ConfigKey<List<string>> LibsKey = new("Libs", "Paths to check for dependency libraries.", () => new List<string>());
-        public static ConfigKey<List<string>> ModsKey = new("Mods", "Paths to check for mods.", () => new List<string>());
-        public override string Description { get; } = "Contains collections of paths which will be searched for certain resources.";
+        public readonly ConfigKey<string> ConfigsKey = new("Configs", "Paths to check for configuration files.", () => "./MonkeyLoader/Configs");
+        public readonly ConfigKey<string> GamePacksKey = new("GamePacks", "Paths to check for game packs.", () => "./MonkeyLoader/GamePacks");
+        public readonly ConfigKey<string> LibsKey = new("Libs", "Paths to check for dependency libraries.", () => "./MonkeyLoader/Libs");
+        public readonly ConfigKey<List<ModLoadingLocation>> ModsKey = new("Mods", "Paths to check for mods.", () => new() { new ModLoadingLocation("./MonkeyLoader/Mods", true, "\\.disabled") });
 
+        /// <inheritdoc/>
+        public override string Description { get; } = "Contains definitions for which paths will be searched for certain resources.";
+
+        /// <inheritdoc/>
         public override string Name { get; } = "Locations";
+
+        /// <inheritdoc/>
+        public override Version Version { get; } = new Version(1, 0);
     }
 }
---
+++ MonkeyLoader/Meta/ModLoadingLocation.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Meta
+{
+    /// <summary>
+    /// Specifies where and how to search for mods.
+    /// </summary>
+    [JsonObject]
+    public sealed class ModLoadingLocation
+    {
+        private readonly Regex[] ignorePatterns;
+
+        /// <summary>
+        /// Gets the regex patterns that exclude a mod from being loaded if any match.<br/>
+        /// Patterns are matched case-insensitive.
+        /// </summary>
+        public IEnumerable<Regex> IgnorePatterns
+        {
+            get
+            {
+                foreach (var pattern in ignorePatterns)
+                    yield return pattern;
+            }
+        }
+
+        /// <summary>
+        /// Gets the root folder to search.
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// Gets whether nested folders get searched too.
+        /// </summary>
+        public bool Recursive { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="ModLoadingLocation"/> with the given specification.
+        /// </summary>
+        /// <param name="path">The root folder to search.</param>
+        /// <param name="recursive">Whether to search nested folders too.</param>
+        /// <param name="ignorePatterns">Regular expression patterns that exclude a mod from being loaded if any match.<br/>
+        /// Patterns are matched case-insensitive.</param>
+        [JsonConstructor]
+        public ModLoadingLocation(string path, bool recursive, params string[] ignorePatterns)
+            : this(path, recursive, ignorePatterns.Select(pattern => new Regex(pattern, RegexOptions.IgnoreCase)))
+        { }
+
+        /// <summary>
+        /// Creates a new <see cref="ModLoadingLocation"/> with the given specification.
+        /// </summary>
+        /// <param name="path">The root folder to search.</param>
+        /// <param name="recursive">Whether to search nested folders too.</param>
+        /// <param name="ignorePatterns">Regular expression patterns that exclude a mod from being loaded if any match.<br/>
+        /// Patterns are matched case-insensitive.</param>
+        public ModLoadingLocation(string path, bool recursive, IEnumerable<string> ignorePatterns)
+            : this(path, recursive, ignorePatterns.Select(pattern => new Regex(pattern, RegexOptions.IgnoreCase)))
+        { }
+
+        private ModLoadingLocation(string path, bool recursive, IEnumerable<Regex> ignorePatterns)
+        {
+            Path = path;
+            Recursive = recursive;
+            this.ignorePatterns = ignorePatterns.ToArray();
+        }
+
+        /// <summary>
+        /// Conducts a search based on the specifications of this loading location.
+        /// </summary>
+        /// <returns>The full names (including paths) of all files that satisfy the specifications.</returns>
+        public IEnumerable<string> Search()
+            => Directory.EnumerateFiles(Path, "*.nupkg", Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                    .Where(path => !ignorePatterns.Any(pattern => pattern.IsMatch(path)));
+    }
+}
