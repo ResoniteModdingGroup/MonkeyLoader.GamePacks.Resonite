011bc789ab38c1f6f3612c07cb577a898f1b7cd4
Arne Kiesewetter <banane9@live.de>
2023-10-28 22:57:24 +0200

    Add direct logging methods to monkeybase<monkey>

--- MonkeyLoader/Patching/MonkeyBase.cs
+++ MonkeyLoader/Patching/MonkeyBase.cs
@@ -242,5 +242,42 @@ namespace MonkeyLoader.Patching
             if (Instance is not null)
                 throw new InvalidOperationException("Can't create more than one patcher instance!");
         }
+
+        /// <summary>
+        /// Logs events considered to be useful during debugging when more granular information is needed.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Debug(Func<object> messageProducer) => Logger.Debug(messageProducer);
+
+        /// <summary>
+        /// Logs that one or more functionalities are not working, preventing some from working correctly.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Error(Func<object> messageProducer) => Logger.Error(messageProducer);
+
+        /// <summary>
+        /// Logs that one or more key functionalities, or the whole system isn't working.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Fatal(Func<object> messageProducer) => Logger.Fatal(messageProducer);
+
+        /// <summary>
+        /// Logs that something happened, which is purely informative and can be ignored during normal use.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Info(Func<object> messageProducer) => Logger.Info(messageProducer);
+
+        /// <summary>
+        /// Logs step by step execution of code that can be ignored during standard operation,
+        /// but may be useful during extended debugging sessions.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Trace(Func<object> messageProducer) => Logger.Trace(messageProducer);
+
+        /// <summary>
+        /// Logs that unexpected behavior happened, but work is continuing and the key functionalities are operating as expected.
+        /// </summary>
+        /// <param name="messageProducer">The producer to log if possible.</param>
+        public void Warn(Func<object> messageProducer) => Logger.Warn(messageProducer);
     }
 }
