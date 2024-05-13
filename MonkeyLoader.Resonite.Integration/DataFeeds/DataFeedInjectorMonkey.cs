3ede5d4890af7d9c5f3cf32a4750a0d4a5f1634c
Arne Kiesewetter <banane9@live.de>
2024-05-13 02:29:17 +0200

    Create generic DataFeedInjector base

---
+++ MonkeyLoader.Resonite.Integration/DataFeeds/DataFeedInjectorMonkey.cs
@@ -0,0 +1,36 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite.DataFeeds
+{
+    internal abstract class DataFeedInjectorMonkey<TMonkey, TDataFeed>
+            : ResoniteMonkey<TMonkey>, IAsyncEventSource<EnumerateDataFeedEvent<TDataFeed>>
+        where TMonkey : DataFeedInjectorMonkey<TMonkey, TDataFeed>, new()
+        where TDataFeed : IDataFeed
+    {
+        private static AsyncEventDispatching<EnumerateDataFeedEvent<TDataFeed>>? _dispatching;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(IDataFeed.Enumerate), new[] { typeof(IReadOnlyList<string>), typeof(IReadOnlyList<string>), typeof(string) })]
+        private static IAsyncEnumerable<DataFeedItem> EnumeratePostfix(IAsyncEnumerable<DataFeedItem> __result, TDataFeed __instance, IReadOnlyList<string> path, IReadOnlyList<string> groupKeys, string searchPhrase)
+        {
+            var eventData = new EnumerateDataFeedEvent<TDataFeed>(__instance, __result, path, groupKeys, searchPhrase);
+
+            _dispatching?.Invoke(eventData);
+
+            return eventData.Result;
+        }
+
+        event AsyncEventDispatching<EnumerateDataFeedEvent<TDataFeed>>? IAsyncEventSource<EnumerateDataFeedEvent<TDataFeed>>.Dispatching
+        {
+            add => _dispatching += value;
+            remove => _dispatching -= value;
+        }
+    }
+}
