a7cdfa05dbebc651c71f1cb0a2110c1b1195e758
Nytra <14206961+Nytra@users.noreply.github.com>
2024-06-17 21:09:59 +0100

    Reset field value if config key set fails

--- MonkeyLoader.Resonite.Integration/FieldExtensions.cs
+++ MonkeyLoader.Resonite.Integration/FieldExtensions.cs
@@ -19,10 +19,10 @@ namespace MonkeyLoader.Resonite
             void FieldChangedHandler(IChangeable _)
             {
                 if (field.Value.Equals(configKey.GetValue())) return;
-                if (allowWriteBack)
-                    configKey.SetValue(field.Value, eventLabel);
-                else
+                if (!(allowWriteBack && configKey.TrySetValue(field.Value, eventLabel)))
+                {
                     field.World.RunSynchronously(() => field.Value = configKey.GetValue()!);
+                }
             }
             void ConfigKeyChangedHandler(object sender, ConfigKeyChangedEventArgs<T> args)
             {
