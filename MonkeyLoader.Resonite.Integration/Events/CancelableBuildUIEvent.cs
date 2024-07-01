ce2325eab2ddd01c4251abba25d194da973682aa
Arne Kiesewetter <banane9@live.de>
2024-07-01 19:00:13 +0200

    Add cancelable base events

---
+++ MonkeyLoader.Resonite.Integration/Events/CancelableBuildUIEvent.cs
@@ -0,0 +1,30 @@
+﻿using FrooxEngine.UIX;
+using MonkeyLoader.Events;
+
+namespace MonkeyLoader.Resonite.Events
+{
+    /// <summary>
+    /// Abstract base class for all sorts of cancelable UI-generation events.
+    /// </summary>
+    public abstract class CancelableBuildUIEvent : CancelableSyncEvent
+    {
+        /// <summary>
+        /// Gets the <see cref="UIBuilder"/> that should be used to generate extra UI elements.
+        /// </summary>
+        /// <remarks>
+        /// The style and <see cref="UIBuilder.Current"/> target should be the same after handling the event.<br/>
+        /// Use <see cref="UIBuilder.PushStyle"/> before making style changes,
+        /// and revert them by using <see cref="UIBuilder.PopStyle"/> once done.
+        /// </remarks>
+        public UIBuilder UI { get; }
+
+        /// <summary>
+        /// Creates a new UI-generation event instance with the given <see cref="UIBuilder"/>.
+        /// </summary>
+        /// <param name="ui">The <see cref="UIBuilder"/> to use while generating extra UI elements.</param>
+        protected CancelableBuildUIEvent(UIBuilder ui)
+        {
+            UI = ui;
+        }
+    }
+}
