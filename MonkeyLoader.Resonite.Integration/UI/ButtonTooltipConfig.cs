c4b41d70ea22c0692be29a020e9a69bbc3b500f3
Arne Kiesewetter <banane9@live.de>
2024-08-14 20:38:43 +0200

    Add config equivalent to base and monkey definition

---
+++ MonkeyLoader.Resonite.Integration/UI/ButtonTooltipConfig.cs
@@ -0,0 +1,21 @@
+﻿using Elements.Core;
+using MonkeyLoader.Configuration;
+using System;
+
+namespace MonkeyLoader.Resonite.UI
+{
+    internal sealed class ButtonTooltipConfig : ConfigSection
+    {
+        private static readonly DefiningConfigKey<colorX> _bgColor = new("BackgroundColor", "Sets the background color of a tooltip.", () => colorX.Black.SetA(.75f));
+        private static readonly DefiningConfigKey<colorX> _textColor = new("TextColor", "Sets the text color of a tooltip.", () => colorX.White);
+
+        private static readonly DefiningConfigKey<float> _textScale = new("TextSize", "Sets the size of the text on a tooltip.", () => 1)
+        {
+            new ConfigKeyRange<float>(.5f, 4)
+        };
+
+        public override string Description => "Contains settings for the tooltips displayed for buttons.";
+        public override string Id => "Tooltips";
+        public override Version Version { get; } = new Version(1, 0, 0);
+    }
+}
---
+++ MonkeyLoader.Resonite.Integration/UI/ButtonTooltips.cs
@@ -0,0 +1,23 @@
+﻿using FrooxEngine.UIX;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite.UI
+{
+    /// <summary>
+    /// Adds tooltips to <see cref="Button"/>s,
+    /// similar to how they were originally done for Neos by Psychpsyo.<br/>
+    /// <see href="https://github.com/Psychpsyo/Tooltippery"/>
+    /// </summary>
+    [HarmonyPatch(typeof(Button))]
+    [HarmonyPatchCategory(nameof(ButtonTooltips))]
+    internal sealed class ButtonTooltips : ConfiguredResoniteMonkey<ButtonTooltips, ButtonTooltipConfig>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => [];
+    }
+}
