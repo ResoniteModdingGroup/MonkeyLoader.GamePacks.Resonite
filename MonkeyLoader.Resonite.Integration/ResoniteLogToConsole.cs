a0b171d0ee7a6ac1bb3a33519aac4960bfa53bf0
Arne Kiesewetter <banane9@live.de>
2024-10-03 01:14:40 +0200

    Add resonite log to console monkey

---
+++ MonkeyLoader.Resonite.Integration/ResoniteLogToConsole.cs
@@ -0,0 +1,63 @@
+﻿using Elements.Core;
+using MonkeyLoader.Logging;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite
+{
+    internal sealed class ResoniteLogToConsole : Monkey<ResoniteLogToConsole>
+    {
+        public override bool CanBeDisabled => true;
+
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => [];
+
+        protected override bool OnLoaded()
+        {
+            UniLog.OnError += message => ToConsoleLoggingHandler(LoggingLevel.Error, message);
+            UniLog.OnWarning += message => ToConsoleLoggingHandler(LoggingLevel.Warn, message);
+            UniLog.OnLog += message => ToConsoleLoggingHandler(LoggingLevel.Info, message);
+
+            return true;
+        }
+
+        private static void ToConsoleLoggingHandler(LoggingLevel level, string message)
+        {
+            Task.Run(() =>
+            {
+                // Log when enabled, Console is open, level is supported, and message isn't from the ModLoader
+                if (!Enabled || !ConsoleLoggingHandler.Instance.Connected || !Logger.ShouldLog(level) || message.Length <= 25 || message.Contains("[MonkeyLoader"))
+                    return;
+
+                string Producer()
+                {
+                    var fpsIndex = message.IndexOf(" FPS)");
+                    if (fpsIndex >= 0)
+                    {
+                        var openIndex = message.LastIndexOf('(', fpsIndex - 4);
+                        message = message[openIndex..];
+                    }
+
+                    return message.TrimEnd('\r', '\n');
+                }
+
+                switch (level)
+                {
+                    case LoggingLevel.Error:
+                        ConsoleLoggingHandler.Instance.Error(Producer);
+                        break;
+
+                    case LoggingLevel.Warn:
+                        ConsoleLoggingHandler.Instance.Warn(Producer);
+                        break;
+
+                    default:
+                        ConsoleLoggingHandler.Instance.Info(Producer);
+                        break;
+                }
+            });
+        }
+    }
+}
