31e4bc81a820ee59f7f95f108ae531236080de1a
Arne Kiesewetter <banane9@live.de>
2024-12-20 01:40:10 +0100

    Move offset range component to be shared for other additions too

--- MonkeyLoader.Resonite.Integration/UI/DefaultInspectorHeaderConfig.cs
+++ MonkeyLoader.Resonite.Integration/UI/DefaultInspectorHeaderConfig.cs
@@ -1,4 +1,5 @@
-﻿using MonkeyLoader.Configuration;
+﻿using FrooxEngine;
+using MonkeyLoader.Configuration;
 using MonkeyLoader.Resonite.Configuration;
 using MonkeyLoader.Resonite.UI.Inspectors;
 using System;
@@ -10,29 +11,34 @@ namespace MonkeyLoader.Resonite.UI
     /// </summary>
     public sealed class DefaultInspectorHeaderConfig : SingletonConfigSection<DefaultInspectorHeaderConfig>
     {
-        private static readonly DefiningConfigKey<int> _destroyOffset = new("DestroyOffset", "The Order Offset of the Destroy button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 12)
+        private readonly DefiningConfigKey<int> _destroyOffset = new("DestroyOffset", "The Order Offset of the Destroy button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 12)
         {
-            new ConfigKeyRange<int>(0, 16),
+            OffsetRange,
             new ConfigKeySessionShare<int, long>(IntToLong, LongToInt, 12)
         };
 
-        private static readonly DefiningConfigKey<int> _duplicateOffset = new("DuplicateOffset", "The Order Offset of the Duplicate button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 11)
+        private readonly DefiningConfigKey<int> _duplicateOffset = new("DuplicateOffset", "The Order Offset of the Duplicate button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 11)
         {
-            new ConfigKeyRange<int>(0, 16),
+            OffsetRange,
             new ConfigKeySessionShare<int, long>(IntToLong, LongToInt, 11)
         };
 
-        private static readonly DefiningConfigKey<int> _openContainerOffset = new("OpenContainerOffset", "The Order Offset of the Open Container button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 4)
+        private readonly DefiningConfigKey<int> _openContainerOffset = new("OpenContainerOffset", "The Order Offset of the Open Container button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 4)
         {
-            new ConfigKeyRange<int>(0, 16),
+            OffsetRange,
             new ConfigKeySessionShare<int, long>(IntToLong, LongToInt, 10)
         };
 
         private readonly DefiningConfigKey<int> _workerNameOffset = new("NameOffset", "The Order Offset of the Worker Name button on Inspector Headers. Range: 0-16 - Higher is further right.", () => 6)
         {
-            new ConfigKeyRange<int>(0, 16),
+            OffsetRange,
             new ConfigKeySessionShare<int, long>(IntToLong, LongToInt, 6)
         };
+
+        /// <summary>
+        /// Gets the range component used for <see cref="WorkerInspector"/> header items.
+        /// </summary>
+        public static ConfigKeyRange<int> OffsetRange { get; } = new ConfigKeyRange<int>(0, 16);
 
         /// <inheritdoc/>
         public override string Description => "Options for the default inspector header generation.";
