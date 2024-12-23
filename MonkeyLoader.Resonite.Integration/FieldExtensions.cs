3f3be3f9bf05044b10a92822e3c3fef06cf73b01
Arne Kiesewetter <banane9@live.de>
2024-12-23 01:31:56 +0100

    Rework enum sync field extension into nullable<T> Value and HasValue sync options

--- MonkeyLoader.Resonite.Integration/FieldExtensions.cs
+++ MonkeyLoader.Resonite.Integration/FieldExtensions.cs
@@ -17,6 +17,16 @@ namespace MonkeyLoader.Resonite
     public static class FieldExtensions
     {
         /// <summary>
+        /// The prefix for the <see cref="IDefiningConfigKey{T}.SetValue(T, string?)">SetValue</see>
+        /// <c>eventLabel</c> used when the cause is a change in the synchronized <see cref="IField{T}">field</see>'s shared value.
+        /// </summary>
+        /// <remarks>
+        /// The actually passed label has the following format:
+        /// <c>$"{<see cref="WriteBackPrefix"/>}.{<see cref="ValueField{T}">field</see>.<see cref="World"/>.<see cref="GetIdentifier">GetIdentifier</see>()}"</c>
+        /// </remarks>
+        public const string WriteBackPrefix = "SyncedField.WriteBack";
+
+        /// <summary>
         /// Creates a label describing the <paramref name="target"/> reference as a <see cref="RefEditor"/> would.
         /// </summary>
         /// <param name="target">The reference to label.</param>
@@ -37,7 +47,7 @@ namespace MonkeyLoader.Resonite
         }
 
         /// <summary>
-        /// Synchronizes the <paramref name="field"/>'s label with that of the <paramref name="configKey"/>.
+        /// Synchronizes the <paramref name="field"/>'s <see cref="IValue{T}.Value"/> with that of the <paramref name="configKey"/>.
         /// </summary>
         /// <remarks>
         /// <para>
@@ -62,76 +72,138 @@ namespace MonkeyLoader.Resonite
         /// </param>
         /// <param name="allowWriteBack">Whether changes of the <paramref name="field"/> should be written back to the config key.</param>
         /// <returns>The delegate subscribed to the <paramref name="field"/>'s <see cref="IChangeable.Changed">Changed</see> event.</returns>
-        public static Action<IChangeable> SyncWithConfigKey<T>(this IField<T> field, IDefiningConfigKey<T> configKey, string? eventLabel = null, bool allowWriteBack = true)
+        public static Action<IChangeable> SyncWithConfigKey<T>(this IField<T> field,
+            IDefiningConfigKey<T> configKey, string? eventLabel = null, bool allowWriteBack = true)
         {
             field.Value = configKey.GetValue()!;
-            eventLabel ??= $"SyncedField.WriteBack.{field.World.GetIdentifier()}";
+            eventLabel ??= field.GetWriteBackEventLabel();
+
+            var parent = field.FindNearestParent<Component>();
+
+            void ParentDestroyedHandler(IDestroyable _)
+            {
+                parent.Destroyed -= ParentDestroyedHandler;
+
+                field.Changed -= FieldChangedHandler;
+                configKey.Changed -= ConfigKeyChangedHandler;
+            }
 
             void FieldChangedHandler(IChangeable _)
             {
-                if (Equals(field.Value, configKey.GetValue()))
-                    return;
-
-                if (!allowWriteBack || !configKey.TrySetValue(field.Value, eventLabel))
+                if (Equals(field.Value, configKey.GetValue()) && (!allowWriteBack || !configKey.TrySetValue(field.Value, eventLabel)))
                     field.World.RunSynchronously(() => field.Value = configKey.GetValue()!);
             }
 
             void ConfigKeyChangedHandler(object sender, ConfigKeyChangedEventArgs<T> args)
             {
-                if (field.FilterWorldElement() == null)
-                {
-                    configKey.Changed -= ConfigKeyChangedHandler;
-                    return;
-                }
-
-                if (Equals(field.Value, configKey.GetValue()))
-                    return;
-
-                field.World.RunSynchronously(() => field.Value = configKey.GetValue()!);
+                if (!Equals(field.Value, configKey.GetValue()))
+                    field.World.RunSynchronously(() => field.Value = configKey.GetValue()!);
             }
 
             field.Changed += FieldChangedHandler;
             configKey.Changed += ConfigKeyChangedHandler;
+            parent.Destroyed += ParentDestroyedHandler;
 
             return FieldChangedHandler;
         }
 
-        public static Action<IChangeable> SyncWithNullableEnumConfigKey<T>(this IField<T> field, IDefiningConfigKey configKey, string? eventLabel = null, bool allowWriteBack = true)
+        /// <summary>
+        /// Synchronizes the <paramref name="field"/>'s <see cref="IValue{T}.Value"/> with
+        /// the <see cref="Nullable{T}.HasValue"/> of the <paramref name="configKey"/>.<br/>
+        /// When the field is toggled to <c>true</c> or set to <c>false</c>,
+        /// the <see cref="Nullable{T}.Value"/> is set to <c>default(<typeparamref name="T"/>)</c>.
+        /// </summary>
+        /// <inheritdoc cref="SyncWithConfigKey{T}(IField{T}, IDefiningConfigKey{T}, string?, bool)"/>
+        public static Action<IChangeable> SyncWithNullableConfigKeyHasValue<T>(this IField<bool> field,
+            IDefiningConfigKey<T?> configKey, string? eventLabel = null, bool allowWriteBack = true)
+            where T : unmanaged
         {
-            // support for nullable enums requires this to never be null
-            field.Value = (T)(configKey.GetValue() ?? default(T));
-            eventLabel ??= $"SyncedField.WriteBack.{field.World.GetIdentifier()}";
+            field.Value = configKey.GetValue().HasValue;
+            eventLabel ??= field.GetWriteBackEventLabel();
+
+            var parent = field.FindNearestParent<Component>();
+
+            void ParentDestroyedHandler(IDestroyable _)
+            {
+                parent.Destroyed -= ParentDestroyedHandler;
+
+                field.Changed -= FieldChangedHandler;
+                configKey.Changed -= ConfigKeyChangedHandler;
+            }
 
             void FieldChangedHandler(IChangeable _)
             {
-                if (Equals(field.Value, configKey.GetValue()))
-                    return;
+                T? newValue = field.Value ? default : null;
+                var keyValue = configKey.GetValue().HasValue;
 
-                // this avoids unexpected behavior with nullable flag enums
-                if (configKey.ValueType.IsNullable() && configKey.ValueType.GetGenericArguments()[0].IsEnum && configKey.GetValue() is null && Equals(field.Value, default(T))) return;
-
-                if (!allowWriteBack || !configKey.TrySetValue(field.Value, eventLabel))
-                    field.World.RunSynchronously(() => field.Value = (T)(configKey.GetValue() ?? default(T)));
+                if (field.Value != keyValue && (!allowWriteBack || !configKey.TrySetValue(newValue, eventLabel)))
+                    field.World.RunSynchronously(() => field.Value = keyValue);
             }
 
-            void ConfigKeyChangedHandler(object sender, IConfigKeyChangedEventArgs args)
+            void ConfigKeyChangedHandler(object sender, ConfigKeyChangedEventArgs<T?> args)
             {
-                if (field.FilterWorldElement() == null)
-                {
-                    configKey.Changed -= ConfigKeyChangedHandler;
-                    return;
-                }
+                var keyValue = configKey.GetValue().HasValue;
 
-                if (Equals(field.Value, configKey.GetValue()))
-                    return;
-
-                field.World.RunSynchronously(() => field.Value = (T)(configKey.GetValue() ?? default(T)));
+                if (field.Value != keyValue)
+                    field.World.RunSynchronously(() => field.Value = keyValue);
             }
 
             field.Changed += FieldChangedHandler;
             configKey.Changed += ConfigKeyChangedHandler;
+            parent.Destroyed += ParentDestroyedHandler;
 
             return FieldChangedHandler;
         }
+
+        /// <summary>
+        /// Synchronizes the <paramref name="field"/>'s <see cref="IValue{T}.Value"/> with
+        /// the <see cref="Nullable{T}.Value"/> of the <paramref name="configKey"/>.<br/>
+        /// When the <see cref="Nullable{T}.HasValue"/> is <c>false</c>,
+        /// the <paramref name="field"/> is set to <c>default(<typeparamref name="T"/>)</c>.
+        /// </summary>
+        /// <inheritdoc cref="SyncWithConfigKey{T}(IField{T}, IDefiningConfigKey{T}, string?, bool)"/>
+        public static Action<IChangeable> SyncWithNullableConfigKeyValue<T>(this IField<T> field,
+                IDefiningConfigKey<T?> configKey, string? eventLabel = null, bool allowWriteBack = true)
+            where T : unmanaged
+        {
+            // support for nullables requires this to never be null
+            field.Value = configKey.GetValue() ?? default;
+            eventLabel ??= field.GetWriteBackEventLabel();
+
+            var parent = field.FindNearestParent<Component>();
+
+            void ParentDestroyedHandler(IDestroyable _)
+            {
+                parent.Destroyed -= ParentDestroyedHandler;
+
+                field.Changed -= FieldChangedHandler;
+                configKey.Changed -= ConfigKeyChangedHandler;
+            }
+
+            void FieldChangedHandler(IChangeable _)
+            {
+                var keyValue = configKey.GetValue() ?? default;
+
+                if (!Equals(field.Value, keyValue) && (!allowWriteBack || !configKey.TrySetValue(field.Value, eventLabel)))
+                    field.World.RunSynchronously(() => field.Value = keyValue);
+            }
+
+            void ConfigKeyChangedHandler(object sender, ConfigKeyChangedEventArgs<T?> args)
+            {
+                var keyValue = configKey.GetValue() ?? default;
+
+                if (!Equals(field.Value, keyValue))
+                    field.World.RunSynchronously(() => field.Value = keyValue);
+            }
+
+            field.Changed += FieldChangedHandler;
+            configKey.Changed += ConfigKeyChangedHandler;
+            parent.Destroyed += ParentDestroyedHandler;
+
+            return FieldChangedHandler;
+        }
+
+        private static string GetWriteBackEventLabel(this IField field)
+            => $"SyncedField.WriteBack.{field.World.GetIdentifier()}";
     }
 }
