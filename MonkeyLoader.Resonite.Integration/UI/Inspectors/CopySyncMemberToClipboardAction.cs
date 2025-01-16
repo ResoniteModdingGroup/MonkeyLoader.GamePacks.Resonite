47bc5dff80d76e50e0a688435827b4863fc7a530
Arne Kiesewetter <banane9@live.de>
2025-01-16 20:31:45 +0100

    Add copy to clipboard menu item for IField sync members as demo

---
+++ MonkeyLoader.Resonite.Integration/UI/Inspectors/CopySyncMemberToClipboardAction.cs
@@ -0,0 +1,32 @@
+﻿using FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite.UI.Inspectors
+{
+    internal sealed class CopySyncMemberToClipboardAction
+        : ResoniteAsyncEventHandlerMonkey<CopySyncMemberToClipboardAction, GenerateInspectorMemberActionsMenuItemsAsyncEvent>
+    {
+        public override int Priority => HarmonyLib.Priority.Normal;
+
+        protected override bool AppliesTo(GenerateInspectorMemberActionsMenuItemsAsyncEvent eventData)
+            => base.AppliesTo(eventData) && eventData.Target is IField;
+
+        protected override Task Handle(GenerateInspectorMemberActionsMenuItemsAsyncEvent eventData)
+        {
+            var field = (IField)eventData.Target;
+            var menuItem = eventData.ContextMenu.AddItem("Copy to Clipboard",
+                OfficialAssets.Graphics.Icons.General.Duplicate, RadiantUI_Constants.Sub.GREEN);
+
+            menuItem.Button.LocalPressed += (button, _) =>
+            {
+                button.World.InputInterface.Clipboard.SetText(field.BoxedValue.ToString());
+                button.World.LocalUser.CloseContextMenu(eventData.Instance);
+            };
+
+            return Task.CompletedTask;
+        }
+    }
+}
