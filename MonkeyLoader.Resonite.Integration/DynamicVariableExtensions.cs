2c6923c9e94177c09bdd8f95fadde0cea8b14ce2
Arne Kiesewetter <banane9@live.de>
2025-01-23 20:27:38 +0100

    Add additional Dynamic Variable related extensions

--- MonkeyLoader.Resonite.Integration/DynamicVariableExtensions.cs
+++ MonkeyLoader.Resonite.Integration/DynamicVariableExtensions.cs
@@ -1,4 +1,6 @@
-﻿using FrooxEngine;
+﻿using Elements.Core;
+using FrooxEngine;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -58,6 +60,62 @@ namespace MonkeyLoader.Resonite
         /// <inheritdoc cref="CreateSpace"/>
         public static DynamicVariableSpace FindOrCreateSpace(this Slot slot, string? spaceName, bool onlyDirectBinding = false)
             => slot.FindSpace(spaceName!) ?? CreateSpace(slot, spaceName, onlyDirectBinding);
+
+        /// <summary>
+        /// Gets the <see cref="DynamicVariableSpace"/> that this
+        /// <see cref="IDynamicVariable">dynamic variable</see> is linked to.
+        /// </summary>
+        /// <param name="dynamicVariable">The dynamic variable of which to get the linked <see cref="DynamicVariableSpace"/>.</param>
+        /// <returns>The <see cref="DynamicVariableSpace"/> that this <see cref="IDynamicVariable">dynamic variable</see> is linked to.</returns>
+        /// <exception cref="NullReferenceException">
+        /// When this <see cref="IDynamicVariable">dynamic variable</see> is not linked to a <see cref="DynamicVariableSpace"/>.
+        /// </exception>
+        public static DynamicVariableSpace GetLinkedSpace(this IDynamicVariable dynamicVariable)
+        {
+            if (dynamicVariable.TryGetLinkedSpace(out var space))
+                return space;
+
+            throw new NullReferenceException("Dynamic variable is not linked against a space!");
+        }
+
+        /// <inheritdoc cref="GetLinkedVariables"/>
+        /// <typeparam name="T">The type of the values of the <see cref="IDynamicVariable{T}">dynamic variables</see>.</typeparam>
+        public static IEnumerable<IDynamicVariable<T>> GetLinkedVariables<T>(this DynamicVariableSpace space,
+            Predicate<IDynamicVariable<T>>? filter = null, bool includeLocal = false, bool excludeDisabled = false, Predicate<Slot>? slotFilter = null)
+        {
+            filter ??= Filter;
+
+            return space.Slot.GetComponentsInChildren<IDynamicVariable<T>>(
+                variable => variable.IsLinkedToSpace(space) && filter(variable),
+                includeLocal, excludeDisabled, slotFilter!);
+        }
+
+        /// <summary>
+        /// <see cref="Slot.GetComponentsInChildren{T}(Predicate{T}, bool, bool, Predicate{Slot})">Gets</see>
+        /// all <see cref="IDynamicVariable{T}">dynamic variable</see> components linked to this
+        /// <see cref="DynamicVariableSpace"/> that fulfill the given conditions.
+        /// </summary>
+        /// <param name="space">The dynamic variable space to get the linked variables of.</param>
+        /// <param name="filter">A predicate to filter the linked variables returned; or <c>null</c> for all.</param>
+        /// <param name="includeLocal">Whether to include local <see cref="Slot"/>s in the search.</param>
+        /// <param name="excludeDisabled">Whether to exclude disabled components from the results.</param>
+        /// <param name="slotFilter">
+        /// A predicate to filter the <see cref="Slot"/>s included in the search; or <c>null</c> for all.<br/>
+        /// The children of excluded <see cref="Slot"/>s are excluded as well.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IDynamicVariable{T}">dynamic variable</see> components linked to this
+        /// <see cref="DynamicVariableSpace"/> that fulfill the given conditions.
+        /// </returns>
+        public static IEnumerable<IDynamicVariable> GetLinkedVariables(this DynamicVariableSpace space,
+            Predicate<IDynamicVariable>? filter = null, bool includeLocal = false, bool excludeDisabled = false, Predicate<Slot>? slotFilter = null)
+        {
+            filter ??= Filter;
+
+            return space.Slot.GetComponentsInChildren<IDynamicVariable>(
+                variable => variable.IsLinkedToSpace(space) && filter(variable),
+                includeLocal, excludeDisabled, slotFilter!);
+        }
 
         /// <summary>
         /// Searches for a <see cref="DynamicField{T}"/> targetting this field
@@ -157,6 +215,31 @@ namespace MonkeyLoader.Resonite
         //}
 
         /// <summary>
+        /// Determines whether this <see cref="IDynamicVariable">dynamic variable</see>
+        /// is linked against a <see cref="DynamicVariableSpace"/>.
+        /// </summary>
+        /// <param name="dynamicVariable">The dynamic variable for which to check for a linked <see cref="DynamicVariableSpace"/>.</param>
+        /// <returns>
+        /// <c>true</c> if this <see cref="IDynamicVariable">dynamic variable</see>
+        /// is linked to a <see cref="DynamicVariableSpace"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsLinkedToSpace(this IDynamicVariable dynamicVariable)
+            => dynamicVariable.TryGetLinkedSpace(out _);
+
+        /// <summary>
+        /// Determines whether this <see cref="IDynamicVariable">dynamic variable</see>
+        /// is linked against the given <see cref="DynamicVariableSpace"/>.
+        /// </summary>
+        /// <param name="dynamicVariable">The dynamic variable for which to check the linked <see cref="DynamicVariableSpace"/>.</param>
+        /// <param name="space">The dynamic variable space to compare to the linked one.</param>
+        /// <returns>
+        /// <c>true</c> if this <see cref="IDynamicVariable">dynamic variable</see>
+        /// is linked to the given <see cref="DynamicVariableSpace"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsLinkedToSpace(this IDynamicVariable dynamicVariable, DynamicVariableSpace space)
+            => dynamicVariable.TryGetLinkedSpace(out var linkedSpace) && linkedSpace == space;
+
+        /// <summary>
         /// Tests whether this string is a <see cref="DynamicVariableHelper.IsValidName">valid dynamic variable name</see>.
         /// </summary>
         /// <param name="variableName">The string to test.</param>
@@ -183,5 +266,30 @@ namespace MonkeyLoader.Resonite
         /// <returns>The <see cref="string.Trim()">trimmed</see> name if <see cref="IsValidName">valid</see>; otherwise, <c>null</c>.</returns>
         public static string? ProcessName(this string? variableName)
             => DynamicVariableHelper.ProcessName(variableName!);
+
+        /// <summary>
+        /// Tries to get the <see cref="DynamicVariableSpace"/> that this
+        /// <see cref="IDynamicVariable">dynamic variable</see> is linked to.
+        /// </summary>
+        /// <param name="dynamicVariable">The dynamic variable of which to get the linked <see cref="DynamicVariableSpace"/>.</param>
+        /// <param name="linkedSpace">
+        /// The <see cref="DynamicVariableSpace"/> that this
+        /// <see cref="IDynamicVariable">dynamic variable</see> is linked to; otherwise, <c>null</c>.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if this <see cref="IDynamicVariable">dynamic variable</see>
+        /// is linked to a <see cref="DynamicVariableSpace"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool TryGetLinkedSpace(this IDynamicVariable dynamicVariable, [NotNullWhen(true)] out DynamicVariableSpace? linkedSpace)
+        {
+            linkedSpace = Traverse.Create(dynamicVariable)
+                .Field(nameof(DynamicVariableBase<dummy>.handler))
+                .Field(nameof(DynamicVariableHandler<dummy>._currentSpace))
+                .GetValue<DynamicVariableSpace>();
+
+            return linkedSpace is not null;
+        }
+
+        private static bool Filter<T>(T item) => true;
     }
 }
