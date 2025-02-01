0b251f130224d53a51e259244cce4f6deab5d1e2
Arne Kiesewetter <banane9@live.de>
2025-02-01 15:36:48 +0100

    Add some extra InspectorMemberActions properties

--- MonkeyLoader.Resonite.Integration/UI/Inspectors/InspectorMemberActionsMenuItemsGenerationEvent.cs
+++ MonkeyLoader.Resonite.Integration/UI/Inspectors/InspectorMemberActionsMenuItemsGenerationEvent.cs
@@ -38,10 +38,30 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
         public InspectorMemberActions MemberActions { get; }
 
         /// <summary>
+        /// Gets the <see cref="Target">Target</see>'s parent <see cref="FrooxEngine.Slot"/>.
+        /// </summary>
+        /// <remarks>
+        /// This may be <c>null</c> if <see cref="Target">Target</see>'s parent is a <see cref="UserComponent"/>.
+        /// </remarks>
+        public Slot? Slot { get; }
+
+        /// <summary>
         /// Gets the <see cref="ISyncMember"/> that
         /// the <see cref="ContextMenu">ContextMenu</see> was opened for.
         /// </summary>
         public ISyncMember Target { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Target">Target</see>'s <see cref="Slot">Slot</see>'s
+        /// <see cref="Slot.ActiveUser">ActiveUser</see>, or its parent <see cref="FrooxEngine.User"/>
+        /// if it belongs to a <see cref="UserComponent"/>.
+        /// </summary>
+        /// <remarks>
+        /// When the <see cref="Target">Target</see>'s parent is a <see cref="Component"/>
+        /// and its <see cref="Slot">Slot</see> does not have an <see cref="Slot.ActiveUser">ActiveUser</see>,
+        /// this will be <c>null</c>.
+        /// </remarks>
+        public User? User { get; }
 
         internal InspectorMemberActionsMenuItemsGenerationEvent(
             InspectorMemberActions instance, ButtonEventData buttonEventData, ISyncMember target)
@@ -49,7 +69,10 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
             MemberActions = instance;
             ButtonEventData = buttonEventData;
             Target = target;
+
             ContextMenu = instance.LocalUser.GetUserContextMenu();
+            Slot = target.FindNearestParent<Slot>();
+            User = Slot?.ActiveUser ?? target.FindNearestParent<User>();
         }
     }
 }
