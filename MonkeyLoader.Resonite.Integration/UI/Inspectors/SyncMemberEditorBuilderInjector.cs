9dd39d766ecc919e42edf62b3c830cc84ddd5545
Arne Kiesewetter <banane9@live.de>
2025-05-28 21:25:58 +0200

    Make use of event source monkeys where possible

--- MonkeyLoader.Resonite.Integration/UI/Inspectors/SyncMemberEditorBuilderInjector.cs
+++ MonkeyLoader.Resonite.Integration/UI/Inspectors/SyncMemberEditorBuilderInjector.cs
@@ -14,34 +14,12 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 {
     [HarmonyPatch(typeof(SyncMemberEditorBuilder))]
     [HarmonyPatchCategory(nameof(SyncMemberEditorBuilderInjector))]
-    internal sealed class SyncMemberEditorBuilderInjector : ResoniteMonkey<SyncMemberEditorBuilderInjector>,
-        ICancelableEventSource<BuildArrayEditorEvent>, ICancelableEventSource<BuildBagEditorEvent>,
-        ICancelableEventSource<BuildListEditorEvent>, ICancelableEventSource<BuildPlaybackEditorEvent>,
-        ICancelableEventSource<BuildFieldEditorEvent>, ICancelableEventSource<BuildObjectEditorEvent>
+    internal sealed class SyncMemberEditorBuilderInjector
+        : ResoniteCancelableEventSourceMonkey<SyncMemberEditorBuilderInjector,
+            BuildArrayEditorEvent, BuildBagEditorEvent, BuildListEditorEvent,
+            BuildPlaybackEditorEvent, BuildFieldEditorEvent, BuildObjectEditorEvent>
     {
-        private static CancelableEventDispatching<BuildArrayEditorEvent>? _buildArrayEditor;
-        private static CancelableEventDispatching<BuildBagEditorEvent>? _buildBagEditor;
-        private static CancelableEventDispatching<BuildFieldEditorEvent>? _buildFieldEditor;
-        private static CancelableEventDispatching<BuildListEditorEvent>? _buildListEditor;
-        private static CancelableEventDispatching<BuildObjectEditorEvent>? _buildObjectEditor;
-        private static CancelableEventDispatching<BuildPlaybackEditorEvent>? _buildPlaybackEditor;
-
         public override bool CanBeDisabled => true;
-
-        /// <inheritdoc/>
-        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => [];
-
-        protected override bool OnEngineReady()
-        {
-            Mod.RegisterEventSource<BuildArrayEditorEvent>(this);
-            Mod.RegisterEventSource<BuildBagEditorEvent>(this);
-            Mod.RegisterEventSource<BuildListEditorEvent>(this);
-            Mod.RegisterEventSource<BuildPlaybackEditorEvent>(this);
-            Mod.RegisterEventSource<BuildFieldEditorEvent>(this);
-            Mod.RegisterEventSource<BuildObjectEditorEvent>(this);
-
-            return base.OnEngineReady();
-        }
 
         [HarmonyPrefix]
         [HarmonyPatch(nameof(SyncMemberEditorBuilder.BuildArray))]
@@ -52,7 +30,7 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildArrayEditorEvent(array, name, fieldInfo, ui, labelSize);
 
-            _buildArrayEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
         }
@@ -66,7 +44,7 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildBagEditorEvent(bag, name, fieldInfo, ui);
 
-            _buildBagEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
         }
@@ -80,7 +58,7 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildFieldEditorEvent(field, name, fieldInfo, ui, labelSize);
 
-            _buildFieldEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
         }
@@ -94,7 +72,7 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildListEditorEvent(list, name, fieldInfo, ui);
 
-            _buildListEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
         }
@@ -108,7 +86,7 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildPlaybackEditorEvent(playback, name, fieldInfo, ui, labelSize);
 
-            _buildPlaybackEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
         }
@@ -122,45 +100,9 @@ namespace MonkeyLoader.Resonite.UI.Inspectors
 
             var eventData = new BuildObjectEditorEvent(syncObject, name, fieldInfo, ui, labelSize);
 
-            _buildObjectEditor?.Invoke(eventData);
+            Dispatch(eventData);
 
             return false;
-        }
-
-        event CancelableEventDispatching<BuildArrayEditorEvent>? ICancelableEventSource<BuildArrayEditorEvent>.Dispatching
-        {
-            add => _buildArrayEditor += value;
-            remove => _buildArrayEditor -= value;
-        }
-
-        event CancelableEventDispatching<BuildBagEditorEvent>? ICancelableEventSource<BuildBagEditorEvent>.Dispatching
-        {
-            add => _buildBagEditor += value;
-            remove => _buildBagEditor -= value;
-        }
-
-        event CancelableEventDispatching<BuildPlaybackEditorEvent>? ICancelableEventSource<BuildPlaybackEditorEvent>.Dispatching
-        {
-            add => _buildPlaybackEditor += value;
-            remove => _buildPlaybackEditor -= value;
-        }
-
-        event CancelableEventDispatching<BuildListEditorEvent>? ICancelableEventSource<BuildListEditorEvent>.Dispatching
-        {
-            add => _buildListEditor += value;
-            remove => _buildListEditor -= value;
-        }
-
-        event CancelableEventDispatching<BuildObjectEditorEvent>? ICancelableEventSource<BuildObjectEditorEvent>.Dispatching
-        {
-            add => _buildObjectEditor += value;
-            remove => _buildObjectEditor -= value;
-        }
-
-        event CancelableEventDispatching<BuildFieldEditorEvent>? ICancelableEventSource<BuildFieldEditorEvent>.Dispatching
-        {
-            add => _buildFieldEditor += value;
-            remove => _buildFieldEditor -= value;
         }
     }
 }
