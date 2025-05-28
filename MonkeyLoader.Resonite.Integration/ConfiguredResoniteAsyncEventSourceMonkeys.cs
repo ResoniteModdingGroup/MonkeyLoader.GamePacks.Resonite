000cb1f548d26317ef7c2c039e2e4eaf1d9c2775
Arne Kiesewetter <banane9@live.de>
2025-05-28 21:11:41 +0200

    Add configured (cancelable) (async) event source monkeys

---
+++ MonkeyLoader.Resonite.Integration/ConfiguredResoniteAsyncEventSourceMonkeys.cs
@@ -0,0 +1,204 @@
+﻿using MonkeyLoader.Configuration;
+using MonkeyLoader.Events;
+using MonkeyLoader.Meta;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite
+{
+    /// <summary>
+    /// Represents the base class for patchers that run after ConfiguredResonite's assemblies have been loaded and that hook into the game's lifecycle,
+    /// while using a <c><typeparamref name="TConfigSection"/> <see cref="ConfigSection">ConfigSection</see></c>.<br/>
+    /// Specifically, to act as an <see cref="IAsyncEventSource{TEvent}">async event source</see> for <typeparamref name="TEvent"/>s.
+    /// </summary>
+    /// <typeparam name="TMonkey">The type of the actual patcher.</typeparam>
+    /// <typeparam name="TConfigSection">The type of the config section to load.</typeparam>
+    /// <typeparam name="TEvent">The <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <inheritdoc/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteAsyncEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent1>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : AsyncEvent
+        where TEvent2 : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteAsyncEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent1, TEvent2>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : AsyncEvent
+        where TEvent2 : AsyncEvent
+        where TEvent3 : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteAsyncEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : AsyncEvent
+        where TEvent2 : AsyncEvent
+        where TEvent3 : AsyncEvent
+        where TEvent4 : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteAsyncEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3, TEvent4>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : AsyncEvent
+        where TEvent2 : AsyncEvent
+        where TEvent3 : AsyncEvent
+        where TEvent4 : AsyncEvent
+        where TEvent5 : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <summary>
+    /// Represents the base class for patchers that run after ConfiguredResonite's assemblies have been loaded and that hook into the game's lifecycle,
+    /// while using a <c><typeparamref name="TConfigSection"/> <see cref="ConfigSection">ConfigSection</see></c>.<br/>
+    /// Specifically, to act as an <see cref="IAsyncEventSource{TEvent}">async event source</see> for the <c>TEvent</c> generic parameter(s).
+    /// </summary>
+    /// <typeparam name="TMonkey">The type of the actual patcher.</typeparam>
+    /// <typeparam name="TConfigSection">The type of the config section to load.</typeparam>
+    /// <typeparam name="TEvent1">The first <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent2">The second <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent3">The third <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent4">The fourth <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent5">The fifth <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent6">The sixth <see cref="AsyncEvent"/> type to dispatch.</typeparam>
+    /// <inheritdoc cref="ResoniteMonkey{TMonkey}"/>
+    public abstract class ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6>
+            : ResoniteAsyncEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteAsyncEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : AsyncEvent
+        where TEvent2 : AsyncEvent
+        where TEvent3 : AsyncEvent
+        where TEvent4 : AsyncEvent
+        where TEvent5 : AsyncEvent
+        where TEvent6 : AsyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteAsyncEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+}
---
+++ MonkeyLoader.Resonite.Integration/ConfiguredResoniteCancelableEventSourceMonkeys.cs
@@ -0,0 +1,204 @@
+﻿using MonkeyLoader.Configuration;
+using MonkeyLoader.Events;
+using MonkeyLoader.Meta;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Resonite
+{
+    /// <summary>
+    /// Represents the base class for patchers that run after ConfiguredResonite's assemblies have been loaded and that hook into the game's lifecycle,
+    /// while using a <c><typeparamref name="TConfigSection"/> <see cref="ConfigSection">ConfigSection</see></c>.<br/>
+    /// Specifically, to act as an <see cref="ICancelableEventSource{TEvent}">cancelable event source</see> for <typeparamref name="TEvent"/>s.
+    /// </summary>
+    /// <typeparam name="TMonkey">The type of the actual patcher.</typeparam>
+    /// <typeparam name="TConfigSection">The type of the config section to load.</typeparam>
+    /// <typeparam name="TEvent">The <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <inheritdoc/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteCancelableEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent1>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : CancelableSyncEvent
+        where TEvent2 : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteCancelableEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent1, TEvent2>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : CancelableSyncEvent
+        where TEvent2 : CancelableSyncEvent
+        where TEvent3 : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteCancelableEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : CancelableSyncEvent
+        where TEvent2 : CancelableSyncEvent
+        where TEvent3 : CancelableSyncEvent
+        where TEvent4 : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <inheritdoc cref="ResoniteCancelableEventSourceMonkey{TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6}"/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3, TEvent4>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : CancelableSyncEvent
+        where TEvent2 : CancelableSyncEvent
+        where TEvent3 : CancelableSyncEvent
+        where TEvent4 : CancelableSyncEvent
+        where TEvent5 : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+
+    /// <summary>
+    /// Represents the base class for patchers that run after ConfiguredResonite's assemblies have been loaded and that hook into the game's lifecycle,
+    /// while using a <c><typeparamref name="TConfigSection"/> <see cref="ConfigSection">ConfigSection</see></c>.<br/>
+    /// Specifically, to act as an <see cref="ICancelableEventSource{TEvent}">cancelable event source</see> for the <c>TEvent</c> generic parameter(s).
+    /// </summary>
+    /// <typeparam name="TMonkey">The type of the actual patcher.</typeparam>
+    /// <typeparam name="TConfigSection">The type of the config section to load.</typeparam>
+    /// <typeparam name="TEvent1">The first <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent2">The second <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent3">The third <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent4">The fourth <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent5">The fifth <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <typeparam name="TEvent6">The sixth <see cref="CancelableSyncEvent"/> type to dispatch.</typeparam>
+    /// <inheritdoc cref="ResoniteMonkey{TMonkey}"/>
+    public abstract class ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6>
+            : ResoniteCancelableEventSourceMonkey<TMonkey, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5>, IConfiguredMonkey<TConfigSection>
+        where TMonkey : ConfiguredResoniteCancelableEventSourceMonkey<TMonkey, TConfigSection, TEvent1, TEvent2, TEvent3, TEvent4, TEvent5, TEvent6>, new()
+        where TConfigSection : ConfigSection, new()
+        where TEvent1 : CancelableSyncEvent
+        where TEvent2 : CancelableSyncEvent
+        where TEvent3 : CancelableSyncEvent
+        where TEvent4 : CancelableSyncEvent
+        where TEvent5 : CancelableSyncEvent
+        where TEvent6 : CancelableSyncEvent
+    {
+        /// <inheritdoc cref="ConfiguredMonkey{TMonkey, TConfigSection}.ConfigSection"/>
+        public static TConfigSection ConfigSection { get; private set; } = null!;
+
+        TConfigSection IConfiguredMonkey<TConfigSection>.ConfigSection => ConfigSection;
+        ConfigSection IConfiguredMonkey.ConfigSection => ConfigSection;
+
+        /// <inheritdoc/>
+        protected ConfiguredResoniteCancelableEventSourceMonkey()
+        { }
+
+        /// <inheritdoc cref="ConfiguredResoniteMonkey{TMonkey, TConfigSection}.OnLoaded"/>
+        protected override bool OnLoaded()
+        {
+            ConfigSection = Config.LoadSection<TConfigSection>();
+
+            return base.OnLoaded();
+        }
+    }
+}
