3ee10c5c08965e6c574782a9a83ca3826fcf1c60
goaaats <goatsdev@protonmail.com>
2025-07-09 21:34:20 +0200

    Initial net9 hacks

---
+++ MonkeyLoader.Resonite.Integration/FrooxEngineInitHook.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using MonkeyLoader.Patching;
+using System.Reflection;
+
+namespace MonkeyLoader.Resonite
+{
+    [HarmonyPatchCategory(nameof(FrooxEngineInitHook))]
+    [HarmonyPatch(typeof(Assembly), nameof(Assembly.LoadFrom), typeof(string))]
+    public class FrooxEngineInitHook : Monkey<FrooxEngineInitHook>
+    {
+        public override string Name { get; } = "Init Fix";
+        
+        static bool Prefix(ref Assembly __result, string assemblyFile)
+        {
+            if (assemblyFile.EndsWith("ProtoFlux.Nodes.FrooxEngine.dll"))
+                return false;
+
+            if (assemblyFile.EndsWith("ProtoFluxBindings.dll"))
+                return false;
+
+            return true;
+        }
+    }
+}
