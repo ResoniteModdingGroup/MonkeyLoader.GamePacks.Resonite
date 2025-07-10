5d07b372162b445ab8d20de1d6409bb7b877da87
Arne Kiesewetter <banane9@live.de>
2025-07-10 15:51:34 +0200

    Directly reference ML from Wrapper and add some package tracking probably have to remove that again or handle it more uniformly

--- MonkeyLoaderWrapper/Program.cs
+++ MonkeyLoaderWrapper/Program.cs
@@ -1,61 +1,64 @@
-﻿using System.Reflection;
+﻿using HarmonyLib;
+using MonkeyLoader.NuGet;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-var monkeyLoaderPath = new FileInfo(Path.Combine("MonkeyLoader", "MonkeyLoader.dll")); 
-var resonitePath = new FileInfo("Resonite.dll");
-
-// This ONLY applies to ML assemblies, so what is required to run ML. We don't want to load anything else with this handler.
-AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+internal class Program
 {
-    var name = args.Name.Split(',')[0];
-    
-    var mlPath = Path.Combine(monkeyLoaderPath.DirectoryName!, $"{name}.dll");
-    if (File.Exists(mlPath))
+    private static readonly FileInfo _monkeyLoaderPath = new(Path.Combine("MonkeyLoader", "MonkeyLoader.dll"));
+
+    private static readonly FileInfo _resonitePath = new FileInfo("Resonite.dll");
+
+    private static object? _monkeyLoader;
+
+    static Program()
     {
-        return Assembly.LoadFile(mlPath);
+        // This ONLY applies to ML assemblies, so what is required to run ML. We don't want to load anything else with this handler.
+        AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+        {
+            var name = args.Name.Split(',')[0];
+
+            var mlPath = Path.Combine(_monkeyLoaderPath.DirectoryName!, $"{name}.dll");
+            if (File.Exists(mlPath))
+                return Assembly.LoadFile(mlPath);
+
+            return null;
+        };
     }
-    
-    return null;
-};
 
-var monkeyLoaderAssembly = Assembly.LoadFile(monkeyLoaderPath.FullName);
-var monkeyLoaderType = monkeyLoaderAssembly.GetType("MonkeyLoader.MonkeyLoader"); var types = monkeyLoaderAssembly.GetTypes();
-var loggingLevelType = monkeyLoaderAssembly.GetType("MonkeyLoader.Logging.LoggingLevel");
-var traceLogLevel = Enum.Parse(loggingLevelType!, "Trace");
-
-var monkeyLoaderInstance = Activator.CreateInstance(monkeyLoaderType!, traceLogLevel, "MonkeyLoader/MonkeyLoader.json");
-var fullLoadMethod = monkeyLoaderType!.GetMethod("FullLoad", BindingFlags.Public | BindingFlags.Instance); 
-fullLoadMethod!.Invoke(monkeyLoaderInstance!, null);
-
-var resoniteAssembly = Assembly.LoadFile(resonitePath.FullName);
-
-// If we had the game and ML in a load context, this would not be necessary since it can resolve native libraries with a generic callback
-NativeLibrary.SetDllImportResolver(resoniteAssembly, ResolveNativeLibrary);
-NativeLibrary.SetDllImportResolver(AppDomain.CurrentDomain.GetAssemblies().First(x => x.FullName!.Contains("SteamAudio.NET")), ResolveNativeLibrary);
-
-var resoniteProgramType = resoniteAssembly.GetType("Program");
-var resoniteMainMethod = resoniteProgramType!.GetMethods(BindingFlags.Static | BindingFlags.NonPublic).First();
-
-var mainResult = resoniteMainMethod!.Invoke(null, [args]);
-
-if (mainResult is Task task)
-{
-    await task;
-}
-
-return;
-
-IntPtr ResolveNativeLibrary(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
-{
-    var runtimesPath = Path.Combine(resonitePath.DirectoryName!, "runtimes",
-        RuntimeInformation.RuntimeIdentifier, "native");
-    
-    var libraryPath = Path.Combine(runtimesPath, $"{libraryName}.dll");
-    
-    if (File.Exists(libraryPath))
+    private static async Task Main(string[] args)
     {
-        return NativeLibrary.Load(libraryPath);
+        var monkeyLoader = new MonkeyLoader.MonkeyLoader();
+        _monkeyLoader = monkeyLoader;
+        monkeyLoader.FullLoad();
+
+        var resoniteAssembly = Assembly.LoadFile(_resonitePath.FullName);
+
+        // If we had the game and ML in a load context, this would not be necessary since it can resolve native libraries with a generic callback
+        NativeLibrary.SetDllImportResolver(resoniteAssembly, ResolveNativeLibrary);
+        NativeLibrary.SetDllImportResolver(AppDomain.CurrentDomain.GetAssemblies().First(x => x.FullName!.Contains("SteamAudio.NET")), ResolveNativeLibrary);
+
+        var resoniteProgramType = resoniteAssembly.GetType("Program");
+        var mainResult = Traverse.Create(resoniteProgramType).Method(nameof(Main), args).GetValue();
+
+        if (mainResult is Task task)
+            await task;
+
+        return;
     }
-    
-    return IntPtr.Zero;
+
+    private static IntPtr ResolveNativeLibrary(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        var runtimesPath = Path.Combine(_resonitePath.DirectoryName!, "runtimes",
+            RuntimeInformation.RuntimeIdentifier, "native");
+
+        var libraryPath = Path.Combine(runtimesPath, $"{libraryName}.dll");
+
+        if (File.Exists(libraryPath))
+            return NativeLibrary.Load(libraryPath);
+
+        return IntPtr.Zero;
+    }
 }
