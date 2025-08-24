cdc17125a3ad5a86e8ceb792158b8c57f63bd1e7
Arne Kiesewetter <banane9@live.de>
2025-08-24 18:29:10 +0200

    Change loading location for system.management

--- MonkeyLoaderWrapper/Program.cs
+++ MonkeyLoaderWrapper/Program.cs
@@ -3,22 +3,51 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
+internal class MonkeyLoaderAssemblyLoadContext(
+    string monkeyLoaderPath,
+    MonkeyLoaderAssemblyLoadContext.AssemblyResolveEventHandler handler)
+    : AssemblyLoadContext("MonkeyLoader")
+{
+    private readonly AssemblyResolveEventHandler? _assemblyResolveEventHandler = handler;
+
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        Debug.WriteLine($"MonkeyLoaderAssemblyLoadContext: Resolving {assemblyName.FullName}");
+
+        if (_assemblyResolveEventHandler != null)
+        {
+            var resolvedAssembly = _assemblyResolveEventHandler(assemblyName);
+            if (resolvedAssembly != null)
+            {
+                Debug.WriteLine($"=> Resolved assembly: {resolvedAssembly.FullName}");
+                return resolvedAssembly;
+            }
+        }
+
+        var name = assemblyName.Name;
+        var mlPath = Path.Combine(monkeyLoaderPath, $"{name}.dll");
+        return File.Exists(mlPath) ? LoadFromAssemblyPath(mlPath) : null;
+    }
+
+    public delegate Assembly? AssemblyResolveEventHandler(AssemblyName assemblyName);
+}
+
 internal class Program
 {
     private static readonly FileInfo _monkeyLoaderPath = new(Path.Combine("MonkeyLoader", "MonkeyLoader.dll"));
 
     private static readonly FileInfo _resonitePath = new("Renderite.Host.dll");
-    
+
     private static object? _monkeyLoaderInstance = null;
     private static MethodInfo? _monkeyLoaderResolveAssemblyMethod = null;
-    
+
     private static async Task Main(string[] args)
     {
         var loadContext = new MonkeyLoaderAssemblyLoadContext(_monkeyLoaderPath.DirectoryName!, (assemblyName) =>
         {
             if (_monkeyLoaderInstance == null || _monkeyLoaderResolveAssemblyMethod == null)
                 return null;
-            
+
             // Attempt to resolve the assembly using MonkeyLoader's method
             var resolvedAssembly = _monkeyLoaderResolveAssemblyMethod.Invoke(_monkeyLoaderInstance, [assemblyName]);
             if (resolvedAssembly is Assembly assembly)
@@ -34,22 +63,24 @@ internal class Program
 
         // https://github.com/dotnet/runtime/blob/main/docs/design/features/AssemblyLoadContext.ContextualReflection.md
         using var contextualReflection = loadContext.EnterContextualReflection();
-        
+
         var monkeyLoaderAssembly = loadContext.LoadFromAssemblyPath(_monkeyLoaderPath.FullName);
 
         // this is a hack
-        var runtimeIdentifierFixed = RuntimeInformation.RuntimeIdentifier.StartsWith("win") ? "win" : RuntimeInformation.RuntimeIdentifier;
-        var systemManagementPath = new FileInfo(Path.Combine("runtimes", runtimeIdentifierFixed, "lib", "net9.0", "System.Management.dll"));
-        if (systemManagementPath.Exists) 
+        var systemManagementPath = RuntimeInformation.RuntimeIdentifier.StartsWith("win")
+            ? new FileInfo(Path.Combine("runtimes", "win", "lib", "net9.0", "System.Management.dll"))
+            : new FileInfo("System.Management.dll");
+
+        if (systemManagementPath.Exists)
             loadContext.LoadFromAssemblyPath(systemManagementPath.FullName);
-        
+
         var monkeyLoaderType = monkeyLoaderAssembly.GetType("MonkeyLoader.MonkeyLoader");
         var loggingLevelType = monkeyLoaderAssembly.GetType("MonkeyLoader.Logging.LoggingLevel");
         var traceLogLevel = Enum.Parse(loggingLevelType!, "Trace");
 
         _monkeyLoaderInstance = Activator.CreateInstance(monkeyLoaderType!, traceLogLevel, "MonkeyLoader/MonkeyLoader.json");
         _monkeyLoaderResolveAssemblyMethod = monkeyLoaderType!.GetMethod("ResolveAssemblyFromPoolsAndMods", BindingFlags.Public | BindingFlags.Instance);
-        var fullLoadMethod = monkeyLoaderType!.GetMethod("FullLoad", BindingFlags.Public | BindingFlags.Instance); 
+        var fullLoadMethod = monkeyLoaderType!.GetMethod("FullLoad", BindingFlags.Public | BindingFlags.Instance);
         fullLoadMethod!.Invoke(_monkeyLoaderInstance!, null);
 
         var resoniteAssembly = loadContext.LoadFromAssemblyPath(_resonitePath.FullName);
@@ -75,34 +106,5 @@ internal class Program
             return NativeLibrary.Load(libraryPath);
 
         return IntPtr.Zero;
-    }
-}
-
-internal class MonkeyLoaderAssemblyLoadContext(
-    string monkeyLoaderPath,
-    MonkeyLoaderAssemblyLoadContext.AssemblyResolveEventHandler handler)
-    : AssemblyLoadContext("MonkeyLoader")
-{
-    public delegate Assembly? AssemblyResolveEventHandler(AssemblyName assemblyName);
-    
-    private readonly AssemblyResolveEventHandler? _assemblyResolveEventHandler = handler;
-
-    protected override Assembly? Load(AssemblyName assemblyName)
-    {
-        Debug.WriteLine($"MonkeyLoaderAssemblyLoadContext: Resolving {assemblyName.FullName}");
-
-        if (_assemblyResolveEventHandler != null)
-        {
-            var resolvedAssembly = _assemblyResolveEventHandler(assemblyName);
-            if (resolvedAssembly != null)
-            {
-                Debug.WriteLine($"=> Resolved assembly: {resolvedAssembly.FullName}");
-                return resolvedAssembly;
-            }
-        }
-        
-        var name = assemblyName.Name;
-        var mlPath = Path.Combine(monkeyLoaderPath, $"{name}.dll");
-        return File.Exists(mlPath) ? LoadFromAssemblyPath(mlPath) : null;
     }
 }
