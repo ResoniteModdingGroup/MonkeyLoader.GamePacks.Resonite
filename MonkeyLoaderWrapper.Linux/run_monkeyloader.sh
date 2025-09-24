0bae9549ef6fdd589605ef81dae0d69c626c00ae
Nytra <14206961+Nytra@users.noreply.github.com>
2025-09-24 12:30:39 +0100

    Add fixing tab

--- MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
+++ MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sed -i '/^# ~ Launch Resonite! :) ~$/c\
+sed -i '/^	# ~ Launch Resonite! :) ~$/c\
 if [[ "$*" != *"--hookfxr-disable"* ]]; then\
     dotnet MonkeyLoaderWrapper.Linux.dll "$@"\
     exit 0\
