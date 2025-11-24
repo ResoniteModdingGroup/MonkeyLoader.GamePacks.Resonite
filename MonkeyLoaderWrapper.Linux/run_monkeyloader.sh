98633e79e633d0df46590666af9c751c8770d166
Phil <philip.schaefer0@gmail.com>
2025-11-24 20:37:12 +0100

    Change shebang to bash in LinuxBootstrap.sh
    Updates shebang in LinuxBootstrap.sh to use bash syntax.

--- MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
+++ MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# change shebang in LinuxBootstrap.sh to support bash syntax
+sed -i '1s|/usr/bin/env sh|/usr/bin/env bash|' ./LinuxBootstrap.sh
+
 sed -i '/^	# ~ Launch Resonite! :) ~$/c\
 if [[ "$*" != *"--hookfxr-disable"* ]]; then\
     dotnet MonkeyLoaderWrapper.Linux.dll "$@"\
