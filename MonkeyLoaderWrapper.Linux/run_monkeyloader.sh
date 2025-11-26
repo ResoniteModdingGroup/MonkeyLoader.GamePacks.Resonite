defc54cee617be492964ee51659a82bb9b8a07b3
Colin Tim Barndt <contact@colintimbarndt.de>
2025-11-26 03:27:36 +0100

    Linux script in one file

--- MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
+++ MonkeyLoaderWrapper.Linux/run_monkeyloader.sh
@@ -2,10 +2,37 @@
 set -e
 
 BOOTSTRAP_SCRIPT="LinuxBootstrap.sh"
-SEARCH='Renderite.Host.dll'
-REPLACE='"$(./GetRenderite.sh "$@")"'
 
-grep -q "$SEARCH" "$BOOTSTRAP_SCRIPT"\
-&& sed -i 's:\b'"$SEARCH"'\b:'"$REPLACE"':g' "$BOOTSTRAP_SCRIPT"
+if ! grep -q "MonkeyLoader" "$BOOTSTRAP_SCRIPT"; then
+    SEARCH='Renderite.Host.dll'
+    REPLACE='"$(
+        RENDERITE="MonkeyLoaderWrapper.Linux.dll"
+        for arg in "$@"; do
+            if [ "$arg" = "--hookfxr-disable" ]; then
+                RENDERITE="Renderite.Host.dll"
+                break
+            fi
+        done
+
+        echo -n "$RENDERITE"
+    )"'
+    # Replace all occurences of SEARCH with REPLACE
+    NEW="$(awk -v search="$SEARCH" -v repl="$REPLACE" '
+    {
+        # Search with word-boundary separation
+        pattern = "\\<" search "\\>"
+        out = ""
+        start = 1
+        while ( match(substr($0, start), pattern) ) {
+            before = substr($0, start, RSTART - 1)
+            out = out before repl
+            start += RSTART + RLENGTH - 1
+        }
+        out = out substr($0, start)
+        print out
+    }
+    ' "$BOOTSTRAP_SCRIPT")"
+    echo -n "$NEW" > "$BOOTSTRAP_SCRIPT"
+fi
 
 "$@"
