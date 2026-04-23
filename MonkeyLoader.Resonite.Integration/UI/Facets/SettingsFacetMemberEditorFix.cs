608c0a7f7140f580fafcea9c476db311338f1373
Arne Kiesewetter <banane9@live.de>
2026-04-23 17:22:11 +0200

    Add fix for missing setup on member editors in settings facet - fixes #171
    Adds a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/2096

---
+++ MonkeyLoader.Resonite.Integration/UI/Facets/SettingsFacetMemberEditorFix.cs
@@ -0,0 +1,53 @@
+﻿using FrooxEngine;
+
+namespace MonkeyLoader.Resonite.UI.Facets
+{
+    internal sealed class SettingsFacetMemberEditorFix : ResoniteEventHandlerMonkey<SettingsFacetMemberEditorFix, FacetPresetLoadedEvent>
+    {
+        /// <summary>
+        /// Defines the text of the Comment used to indicate that this fix has been applied.<br/>
+        /// This must not be changed, as it may be applied multiple times otherwise.
+        /// </summary>
+        private const string FixedMarker = "Editors Fixed";
+
+        public override bool CanBeDisabled => true;
+
+        public override int Priority => HarmonyLib.Priority.Normal;
+
+        protected override bool AppliesTo(FacetPresetLoadedEvent eventData)
+            => base.AppliesTo(eventData)
+            && eventData.FacetPreset is SettingsFacetPreset
+            && eventData is not TemplateFacetPresetFallbackBuiltEvent
+            && eventData.Facet.Slot.GetComponent<Comment>(comment => comment.Text == FixedMarker) is null;
+
+        protected override void Handle(FacetPresetLoadedEvent eventData)
+        {
+            if (eventData.Facet.Slot.GetComponentInChildren<RootCategoryView>() is not RootCategoryView rootCategoryView)
+                return;
+
+            if (rootCategoryView.ItemsManager.TemplateMapper.Target is not DataFeedItemMapper itemMapper)
+                return;
+
+            var templates = itemMapper.Mappings
+                .Select(mapping => mapping.Template.Target?.Slot)
+                .OfType<Slot>();
+
+            foreach (var template in templates)
+            {
+                foreach (var primitiveEditor in template.GetComponentsInChildren<PrimitiveMemberEditor>())
+                {
+                    if (primitiveEditor._textEditor.Target is not TextEditor textEditor)
+                        continue;
+
+                    textEditor.EditingStarted.Target = primitiveEditor.EditingStarted;
+                    textEditor.EditingChanged.Target = primitiveEditor.EditingChanged;
+                    textEditor.EditingFinished.Target = primitiveEditor.EditingFinished;
+                }
+            }
+
+            // Add marker to prevent multiple applications
+            eventData.Facet.Slot.AttachComponent<Comment>().Text.Value = FixedMarker;
+            Logger.Info(() => "Injected editor fix into settings facet!");
+        }
+    }
+}
